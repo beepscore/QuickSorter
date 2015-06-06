db413bca78745a7d8d0dffb13e9ad56c0333be0b
beepscore <support@beepscore.com>
2015-06-06 16:48:26 -0700

    In QuickSorter add calculateMiddleIndex, calculatePivotIndex. Fix partition and quickSort. Now all unit tests pass.

--- app/src/main/java/com/beepscore/android/quicksorter/QuickSorter.java
+++ app/src/main/java/com/beepscore/android/quicksorter/QuickSorter.java
@@ -11,78 +11,78 @@ public class QuickSorter {
     /** Sorts list elements within inclusive range startIndex to endIndex.
      * Sorts supplied list in place.
      * @param list a mutable list of elements that may be compared
-     * @param startIndex to sort entire list use 0
-     * @param endIndex to sort entire list use list.size() - 1
+     * @param lo to sort entire list use 0
+     * @param hi to sort entire list use list.size() - 1
      * Reference
      * https://en.wikipedia.org/wiki/Quicksort
      */
-    public void quickSort(ArrayList<Integer> list, int startIndex, int endIndex) {
+    public void quickSort(ArrayList<Integer> list, int lo, int hi) {
 
-        if (startIndex < endIndex) {
-            Integer partitionIndex = partition(list, startIndex, endIndex);
+        if (lo < hi) {
+            Integer partitionIndex = partition(list, lo, hi);
             // recurse
-            quickSort(list, startIndex, partitionIndex - 1);
-            quickSort(list, partitionIndex + 1, endIndex);
+            quickSort(list, lo, partitionIndex - 1);
+            quickSort(list, partitionIndex + 1, hi);
         }
-
     }
 
-    /** Partition list elements within inclusive range startIndex to endIndex.
+    /** Partition list elements within inclusive range lo to hi.
      * Alters supplied mutable list by sorting elements into two partitions.
      * Small elements on left side and large elements to right side.
      * @param list a mutable list of elements that may be compared
-     * @param startIndex beginning of range to sort
-     * @param endIndex end of range to sort
-     * @return partitionIndex. return endIndex if list is fully sorted
+     * @param lo beginning of range to sort
+     * @param hi end of range to sort
+     * @return partitionIndex. return hi if list is fully sorted
      * return null if list null.
      * Reference
      * https://en.wikipedia.org/wiki/Quicksort
      */
-    public Integer partition(ArrayList<Integer> list, int startIndex, int endIndex) {
+    public Integer partition(ArrayList<Integer> list, int lo, int hi) {
 
         if (list == null) {
-            return endIndex;
+            return hi;
         }
 
         if (list.size() == 1) {
-            return endIndex;
+            return hi;
         }
 
         if (list.size() == 2) {
             if (list.get(0) > list.get(1)) {
                 Collections.swap(list, 0, 1);
             }
-            return endIndex;
+            return hi;
         }
 
-        // TODO: fixme. May want to find pivotIndex
+        // TODO: fixme.
         // list has >= 3 elements
-        // use start + difference to reduce chance of integer overflow (start + end)
-        // integer division truncates
-        int middleIndex = startIndex + ((endIndex - startIndex) / 2);
+        int middleIndex = calculateMiddleIndex(list, lo, hi);
+        int pivotIndex = calculatePivotIndex(list, lo, hi);
+        int pivotValue = list.get(pivotIndex);
 
-        // use median to help avoid worst case behavior if list is already sorted
-        // https://en.wikipedia.org/wiki/Quicksort
-        int pivot = medianOfThreeValues(list.get(startIndex),
-                list.get(middleIndex), list.get(endIndex));
+        // move pivot value to middleIndex if necessary
+        if (list.get(middleIndex) != list.get(pivotIndex)) {
+            Collections.swap(list, middleIndex, pivotIndex);
+        }
 
         // move 2 indices inward, swap as necessary
-        // increment startIndex until find a swappable
-        for (int index = startIndex; index <= middleIndex; ++index) {
-            if (list.get(startIndex) > pivot) {
+        // increment index until find a swappable
+        int index;
+        for (index = lo; index <= middleIndex; ++index) {
+
+            if (list.get(index) > pivotValue) {
 
                 // decrement currentEndIndex until find a swappable
-                for (int currentEndIndex = endIndex; currentEndIndex >= middleIndex; --currentEndIndex) {
+                for (int currentEndIndex = hi; currentEndIndex > middleIndex; --currentEndIndex) {
 
-                    if (list.get(currentEndIndex) < pivot) {
-                        Collections.swap(list, startIndex, currentEndIndex);
+                    if (list.get(currentEndIndex) < pivotValue) {
+                        Collections.swap(list, lo, currentEndIndex);
+                        //storeIndex += 1;
                     }
                 }
-                // ending side has no more values to swap, so starting side is done too
-                break;
             }
         }
-        return middleIndex;
+        return index;
     }
 
     public Integer medianOfThreeValues(Integer a, Integer b, Integer c) {
@@ -98,6 +98,40 @@ public class QuickSorter {
             median = c;
         }
         return median;
+    }
+
+    public int calculateMiddleIndex(ArrayList<Integer> list, int lo, int hi) {
+        // use lo + difference
+        // Reduces chance of integer overflow from (lo + hi)
+        // integer division truncates
+        int middleIndex = lo + ((hi - lo) / 2);
+        return middleIndex;
+    }
+
+    /**
+     * Examines value at lo, hi, and middleIndex
+     * @param list a mutable list of elements that may be compared. list length must be >= 3.
+     * @return index with median value.
+     * This helps avoid worst case behavior if list has duplicates or is already sorted in either order.
+     * https://en.wikipedia.org/wiki/Quicksort
+     */
+    public int calculatePivotIndex(ArrayList<Integer> list, int lo, int hi) {
+        // use start + difference
+        // Reduces chance of integer overflow from (start + end)
+        // integer division truncates
+        int middleIndex = calculateMiddleIndex(list, lo, hi);
+
+        int pivotIndex = middleIndex;
+
+        if (((list.get(lo) > list.get(middleIndex) ) && (list.get(lo) <= list.get(hi)))
+                || ((list.get(lo) < list.get(middleIndex) ) && (list.get(lo) >= list.get(hi)))) {
+            pivotIndex = lo;
+        }
+        else if (((list.get(hi) > list.get(middleIndex) ) && (list.get(hi) <= list.get(lo)))
+                || ((list.get(hi) < list.get(middleIndex) ) && (list.get(hi) >= list.get(lo)))) {
+            pivotIndex = hi;
+        }
+        return pivotIndex;
     }
 
 }
