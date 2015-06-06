3f62793ab2e390bddacc993f1b9b28d2da807b98
beepscore <support@beepscore.com>
2015-06-06 08:45:28 -0700

    In QuickSorterTest add testQuickSorterSortListNull. In QuickSorter add quickSort to make test pass.

--- app/src/androidTest/java/com/beepscore/android/quicksorter/QuickSorterTest.java
+++ app/src/androidTest/java/com/beepscore/android/quicksorter/QuickSorterTest.java
@@ -2,6 +2,8 @@ package com.beepscore.android.quicksorter;
 
 import junit.framework.TestCase;
 
+import java.util.ArrayList;
+
 /**
  * Created by stevebaker on 6/5/15.
  */
@@ -9,14 +11,25 @@ public class QuickSorterTest extends TestCase {
 
     QuickSorter sorter;
 
+    ArrayList<Integer> mList = null;
+
     @Override
     public void setUp() throws Exception {
         super.setUp();
         sorter = new QuickSorter();
+
+        mList = new ArrayList<>();
+        mList.add(5);
     }
 
     public void testQuickSorterNotNull() {
         assertNotNull(sorter);
+    }
+
+    public void testQuickSorterSortListNull() {
+        mList = null;
+        ArrayList<Integer> actual = sorter.quickSort(mList);
+        assertNull(actual);
     }
 
 }
--- app/src/main/java/com/beepscore/android/quicksorter/QuickSorter.java
+++ app/src/main/java/com/beepscore/android/quicksorter/QuickSorter.java
@@ -1,7 +1,21 @@
 package com.beepscore.android.quicksorter;
 
+import java.util.ArrayList;
+
 /**
  * Created by stevebaker on 6/5/15.
  */
 public class QuickSorter {
+
+    /** Does not alter supplied list.
+     *
+     * @param list a list of elements that may be compared
+     * @return a list sorted in increasing order
+     */
+    public ArrayList<Integer> quickSort(ArrayList<Integer> list) {
+        if (list == null) {
+            return null;
+        }
+        return null;
+    }
 }
